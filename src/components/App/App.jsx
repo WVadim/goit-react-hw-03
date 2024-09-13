b16eb04d8002298ae19496ec426838c26125f105
Vadim <vadim.zmiievskyi@gmail.com>
2024-09-13 18:27:40 +0300

    03_2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 import ContactForm from "../ContactForm/ContactForm";
 import ContactList from "../ContactList/ContactList";
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,13 +1,23 @@
 import styles from "./SearchBox.module.css";
 
 const SearchBox = ({ filter, onSearchChange }) => (
-  <input
-    className={styles.search}
-    type="text"
-    value={filter}
-    onChange={onSearchChange}
-    placeholder="Search contacts..."
-  />
+  // <input
+  //   className={styles.search}
+  //   type="text"
+  //   value={filter}
+  //   onChange={onSearchChange}
+  //   placeholder="Search contacts..."
+  // />
+  <div className={styles.searchBox}>
+    <p className={styles.searchP}>Find contact</p>
+    <input
+      className={styles.search}
+      type="text"
+      value={filter}
+      onChange={onSearchChange}
+      placeholder="Search contacts..."
+    />
+  </div>
 );
 
 export default SearchBox;
